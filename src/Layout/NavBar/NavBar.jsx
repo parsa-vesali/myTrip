c5ebc6b555a9bb66a8ba1579672f32e460e05d4d
Parsa Vesali <parsavesali84@gmail.com>
2024-02-26 23:18:19 +0330

    navbar updated

--- src/Layout/NavBar/NavBar.jsx
+++ src/Layout/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FcSportsMode } from "react-icons/fc";
 import SearchIcon from "@mui/icons-material/Search";
 import LoginIcon from "@mui/icons-material/Login";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
@@ -12,92 +13,92 @@ import "./NavBar.css";
 export default function NavBar() {
   return (
     <>
-      <div className="navbar">
-        <div className="navbar__menu">
-          <div className="navbar__menu-right">
-            <div className="navbar__menu-logo">
-              <img src="" alt="logo" />
-              <h2>اسپرت</h2>
-            </div>
-            <div className="navbar__menu-search">
-              <div className="menu-search">
-                <button className="menu-search__icon">
+      <div className="container">
+        <div className="Navbar">
+          <div className="navbar__menu">
+            <div className="navbar__menu-right">
+              <div className="navbar__menu-logo">
+                <FcSportsMode />
+                <h2 className="navbar__menu-logo-title">اسپرت</h2>
+              </div>
+              <div className="navbar__menu-search">
+                <div className="menu-search">
+                  <input
+                    className="menu-search__input"
+                    type="text"
+                    placeholder="جستجوی محصول مورد نظر ..."
+                  />
                   <SearchIcon />
-                </button>
-                <input
-                  className="menu-search__input"
-                  type="text"
-                  placeholder="جستجوی محصول مورد نظر ..."
-                />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="navbar__menu-left">
-            <ul className="navbar__menu-list">
-              <li className="navbar__menu-item">
-                <Link className="navbar__menu-link" to={"/"}>
-                  فروشگاه
-                </Link>
-              </li>
-              <li className="navbar__menu-item">
-                <Link className="navbar__menu-link" to={"/"}>
-                  وبلاگ
-                </Link>
-              </li>
-              <li className="navbar__menu-item">
-                <Link className="navbar__menu-link" to={"/"}>
-                  درباره ما
-                </Link>
-              </li>
-              <li className="navbar__menu-item">
-                <Link className="navbar__menu-link" to={"/"}>
-                  تماس
-                </Link>
-              </li>
-              <button className="navbar__menu-item">
-                <Link className="navbar__menu-link" to={"/"}>
-                  <LoginIcon />
-                  ورود || ثبت نام
-                </Link>
-              </button>
-            </ul>
-          </div>
-        </div>
-        <div className="navbar__products">
-          <div className="navbar__products-right">
-            <ul className="navbar__products-list">
-              <li className="navbar__products-item">
-                <KeyboardArrowDownIcon />
-                کفش ریباک
-              </li>
-              <li className="navbar__products-item">
-                <KeyboardArrowDownIcon />
-                کفش آدیداس
-              </li>
-              <li className="navbar__products-item">
-                <KeyboardArrowDownIcon />
-                کفش رونی
-              </li>
-              <li className="navbar__products-item">
-                <KeyboardArrowDownIcon />
-                کفش نایک
-              </li>
-            </ul>
-          </div>
-          <div className="navbar__products-left">
-            <div className="navbar__products-wallet">
-              <WalletIcon />
-              <span>0 تومان</span>
+            <div className="navbar__menu-left">
+              <ul className="navbar__menu-list">
+                <li className="navbar__menu-item">
+                  <Link className="navbar__menu-link link" to={"/"}>
+                    فروشگاه
+                  </Link>
+                </li>
+                <li className="navbar__menu-item">
+                  <Link className="navbar__menu-link link" to={"/"}>
+                    وبلاگ
+                  </Link>
+                </li>
+                <li className="navbar__menu-item">
+                  <Link className="navbar__menu-link link" to={"/"}>
+                    درباره ما
+                  </Link>
+                </li>
+                <li className="navbar__menu-item">
+                  <Link className="navbar__menu-link link" to={"/"}>
+                    تماس
+                  </Link>
+                </li>
+                <button className="navbar__menu-item navbar__menu-item-account ">
+                  <Link className="navbar__menu-link link" to={"/"}>
+                    <LoginIcon />
+                    ورود || ثبت نام
+                  </Link>
+                </button>
+              </ul>
             </div>
-            <div className="navbar__products-bookmark">
-              <BookmarkIcon />
+          </div>
+          <div className="navbar__products">
+            <div className="navbar__products-right">
+              <ul className="navbar__products-list">
+                <li className="navbar__products-item">
+                  <KeyboardArrowDownIcon />
+                  کفش ریباک
+                </li>
+                <li className="navbar__products-item">
+                  <KeyboardArrowDownIcon />
+                  کفش آدیداس
+                </li>
+                <li className="navbar__products-item">
+                  <KeyboardArrowDownIcon />
+                  کفش رونی
+                </li>
+                <li className="navbar__products-item">
+                  <KeyboardArrowDownIcon />
+                  کفش نایک
+                </li>
+              </ul>
             </div>
-            <div className="navbar__products-favorite">
-              <FavoriteBorderIcon />
-            </div>
-            <div className="navbar__products-support">
-              <HelpIcon />
-              پشتیبان و مشاوره فروش
+            <div className="navbar__products-left">
+              <div className="navbar__products-wallet">
+                <WalletIcon />
+                <span>0 تومان</span>
+              </div>
+              <div className="navbar__products-bookmark">
+                <BookmarkIcon />
+              </div>
+              <div className="navbar__products-favorite">
+                <FavoriteBorderIcon />
+              </div>
+              <div className="navbar__products-support">
+                <HelpIcon />
+                پشتیبان و مشاوره فروش
+              </div>
             </div>
           </div>
         </div>
